79ff86452e3e7c4c113a55aecc3cc81adde456a8
George Benjamin <msdcconnect@gmail.com>
2018-06-22 01:33:26 +0100

    Added settings fragment

---
+++ app/src/main/java/com/kurobarabenjamingeorge/preferences/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.kurobarabenjamingeorge.preferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by George Benjamin on 6/22/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
