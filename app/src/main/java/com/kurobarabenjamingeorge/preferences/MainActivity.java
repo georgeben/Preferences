49a01840041e49a68412fcdc3656185b9970f15f
George Benjamin <msdcconnect@gmail.com>
2018-06-22 01:30:59 +0100

    Set up navigtion to settings activity

--- app/src/main/java/com/kurobarabenjamingeorge/preferences/MainActivity.java
+++ app/src/main/java/com/kurobarabenjamingeorge/preferences/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kurobarabenjamingeorge.preferences;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
